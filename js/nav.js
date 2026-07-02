98e548b4b88e56783a4bc3266e5b1e3988c1c436
Michdo93 <michaeldoerflinger93@gmail.com>
2026-07-02 12:46:26 +0200

    first commit

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+// Aktiven Nav-Link markieren
+document.addEventListener('DOMContentLoaded', () => {
+  const path = location.pathname.split('/').pop();
+  document.querySelectorAll('.topnav a').forEach(a => {
+    const href = a.getAttribute('href').split('/').pop();
+    if (href === path || (path === '' && href === 'index.html')) a.classList.add('active');
+  });
+});
